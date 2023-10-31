68adb1915aa461619914e594fa7daa5780d9fb3f
vipin yadav <vipinyadav31088@gmail.com>
2023-10-31 12:07:48 +0530

    first commit adding all files too the git hub

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Axios from "axios";
+import { useEffect, useState } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        Axios.get("https://jsonplaceholder.typicode.com/posts")
+            .then((Response) => {
+                console.log("getting thing::", Response.data);
+                setData(Response.data);
+            })
+     }, []);
+
+    const arr1 = data.map((data, index) => {
+        return (
+            <tr key = {index}>
+                <td>{data.userId}</td>
+                <td>{data.id}</td>
+                <td>{data.title}</td>
+                <td>{data.body}</td>
+            </tr>
+        );
+    });
+
+    return (
+        <div className="App">
+            <h1>axios is here</h1>
+            <table>
+            <thead>
+                <tr>
+                    <th>UserId</th>
+                    <th>id</th>
+                    <th>title</th>
+                    <th>body</th>
+                  </tr>
+            </thead>
+            <tbody>
+                {arr1}
+        </tbody>
+            </table>
+        </div>
+    );
 }
 
 export default App;
